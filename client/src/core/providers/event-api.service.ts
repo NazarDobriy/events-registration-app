efee2d83289c99dc8a6cd9ec078dee0e8f7b7978
Nazar <nazaranddobriy@gmail.com>
2024-05-15 00:46:07 +0300

    feat: create event api service

---
+++ client/src/core/providers/event-api.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { environment } from 'src/environments/environment';
+import { IEvent } from 'src/pages/home/types/event.interface';
+
+@Injectable()
+export class EventApiService {
+  private url = `${environment.apiHost}/event`;
+
+  constructor(private http: HttpClient) {}
+
+  getEvents(): Observable<IEvent[]> {
+    return this.http.get<IEvent[]>(`${this.url}/all`);
+  }
+
+  getEventById(id: number): Observable<IEvent> {
+    return this.http.get<IEvent>(`${this.url}/${id}`);
+  }
+}
