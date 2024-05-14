553b993a95ed0908431d9775500d5d73477a688c
Nazar <nazaranddobriy@gmail.com>
2024-05-14 14:16:29 +0300

    fix: updates app

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,6 +2,11 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
 
+import { EventModule } from './event/event.module';
+import { Event } from './event/event.model';
+import { ParticipantModule } from './participant/participant.module';
+import { Participant } from './participant/participant.model';
+
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -14,9 +19,11 @@ import { SequelizeModule } from '@nestjs/sequelize';
       username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
-      models: [],
+      models: [Event, Participant],
       autoLoadModels: true
-    })
+    }),
+    EventModule,
+    ParticipantModule
   ],
   controllers: [],
   providers: []
