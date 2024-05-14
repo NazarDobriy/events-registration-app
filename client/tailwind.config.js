2fba65fbfbac00a62db38fe56507be58c8275611
Nazar <nazaranddobriy@gmail.com>
2024-05-15 00:44:49 +0300

    feat: add custom keyframe for loading

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        loading: {
+          "0%": { transform: "rotate(0deg) scale(65%)" },
+          "100%": { transform: "rotate(360deg) scale(100%)" },
+        },
+      },
+      animation: {
+        loading: "loading 2s linear infinite"
+      },
+    },
   },
-  plugins: [],
-}
-
+  plugins: []
+};
