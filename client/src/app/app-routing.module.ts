d8a726ec8ee81db34c8ab18d0941b1a8ebb0d506
Nazar <nazaranddobriy@gmail.com>
2024-05-15 00:52:20 +0300

    feat: create app routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('../pages/home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'events/:id/participants',
+    loadChildren: () =>
+      import('../pages/participants/participants.module').then(
+        (m) => m.ParticipantsModule
+      ),
+  },
+  {
+    path: 'events/:id/register/participant',
+    loadChildren: () =>
+      import('../pages/register-participant/register-participant.module').then(
+        (m) => m.RegisterParticipantModule
+      ),
+  },
+  { path: '**', redirectTo: '/' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
