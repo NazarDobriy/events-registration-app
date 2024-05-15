2dda0dca03ffd4feccd7cc8973b3c96f991efec6
Nazar <nazaranddobriy@gmail.com>
2024-05-16 00:21:20 +0300

    fix: update shared module

--- client/src/shared/shared.module.ts
+++ client/src/shared/shared.module.ts
@@ -1,5 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRadioModule } from '@angular/material/radio';
 
 import { CardComponent } from './components/card/card.component';
 import { LoaderComponent } from './components/loader/loader.component';
@@ -8,6 +15,17 @@ import { CenterWrapperComponent } from './components/center-wrapper/center-wrapp
 @NgModule({
   declarations: [CardComponent, LoaderComponent, CenterWrapperComponent],
   imports: [CommonModule],
-  exports: [CardComponent, LoaderComponent, CenterWrapperComponent]
+  exports: [
+    CardComponent,
+    LoaderComponent,
+    CenterWrapperComponent,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule,
+    MatRadioModule
+  ]
 })
 export class SharedModule {}
