ea4faf245ae4eb027387d6c9cb45901ac01287fa
Nazar <nazaranddobriy@gmail.com>
2024-05-16 00:42:45 +0300

    feat: create error interceptor

--- client/src/core/core.module.ts
+++ client/src/core/core.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { ParticipantApiService } from './providers/participant-api.service';
 import { EventApiService } from './providers/event-api.service';
 import { SnackBarService } from './providers/snack-bar.service';
+import { ErrorInterceptor } from './interceptors/error.interceptor';
 
 @NgModule({
-  providers: [ParticipantApiService, EventApiService, SnackBarService],
-  imports: [CommonModule, MatSnackBarModule],
+  providers: [
+    ParticipantApiService,
+    EventApiService,
+    SnackBarService,
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }
+  ],
+  imports: [CommonModule, MatSnackBarModule]
 })
 export class CoreModule {}
