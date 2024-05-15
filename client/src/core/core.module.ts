a613f47157abe67b48573b2c2a3f5e5157a59ef7
Nazar <nazaranddobriy@gmail.com>
2024-05-16 00:19:40 +0300

    feat: create snack bar service

--- client/src/core/core.module.ts
+++ client/src/core/core.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { ParticipantApiService } from './providers/participant-api.service';
 import { EventApiService } from './providers/event-api.service';
+import { SnackBarService } from './providers/snack-bar.service';
 
 @NgModule({
-  providers: [ParticipantApiService, EventApiService],
-  imports: [CommonModule],
+  providers: [ParticipantApiService, EventApiService, SnackBarService],
+  imports: [CommonModule, MatSnackBarModule],
 })
 export class CoreModule {}
