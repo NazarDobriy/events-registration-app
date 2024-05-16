62a0716e45b1fb2226131273cc551da2450d1d36
Nazar <nazaranddobriy@gmail.com>
2024-05-16 13:52:16 +0300

    fix: handle sotring events

--- client/src/pages/home/home.component.ts
+++ client/src/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MatSelectChange } from '@angular/material/select';
 import { Subject, takeUntil } from 'rxjs';
 
 import { IEvent } from '../../types/event.interface';
@@ -6,7 +7,7 @@ import { EventApiService } from 'src/core/providers/event-api.service';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
+  templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit, OnDestroy {
   isLoading = true;
@@ -17,6 +18,24 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.fetchEvents();
+  }
+
+  handleSort(option: MatSelectChange): void {
+    switch (option.value) {
+      case 'title':
+        this.events.sort((a, b) => a.title.localeCompare(b.title));
+        break;
+      case 'date':
+        this.events.sort((a, b) => {
+          return new Date(a.eventDate).getTime() - new Date(b.eventDate).getTime();
+        });
+        break;
+      case 'organizer':
+        this.events.sort((a, b) => a.organizer.localeCompare(b.organizer));
+        break;
+      default:
+        break;
+    }
   }
 
   private fetchEvents(): void {
