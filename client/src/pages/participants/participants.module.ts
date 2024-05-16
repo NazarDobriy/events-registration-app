b4002ede19c365baf39262046a282c342b9aa6ac
Nazar <nazaranddobriy@gmail.com>
2024-05-16 14:47:58 +0300

    feat: create SearchParticipantsPipe

--- client/src/pages/participants/participants.module.ts
+++ client/src/pages/participants/participants.module.ts
@@ -5,11 +5,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { ParticipantsComponent } from '../participants/participants.component';
 import { ParticipantCardComponent } from './components/participant-card/participant-card.component';
 import { SharedModule } from 'src/shared/shared.module';
+import { SearchParticipantsPipe } from './pipes/search-participants.pipe';
 
 const routes: Routes = [{ path: '', component: ParticipantsComponent }];
 
 @NgModule({
-  declarations: [ParticipantsComponent, ParticipantCardComponent],
-  imports: [CommonModule, SharedModule, RouterModule.forChild(routes)],
+  declarations: [
+    ParticipantsComponent,
+    ParticipantCardComponent,
+    SearchParticipantsPipe
+  ],
+  imports: [CommonModule, SharedModule, RouterModule.forChild(routes)]
 })
 export class ParticipantsModule {}
---
+++ client/src/pages/participants/pipes/search-participants.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { SearchParticipantsPipe } from './search-participants.pipe';
+
+describe('SearchParticipantsPipe', () => {
+  it('create an instance', () => {
+    const pipe = new SearchParticipantsPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
