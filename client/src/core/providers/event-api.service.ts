4e49e0e59427dca4c4ceca68d0c5e19b354e2ecf
Nazar <nazaranddobriy@gmail.com>
2024-05-17 14:50:26 +0300

    fix: route

--- client/src/core/providers/event-api.service.ts
+++ client/src/core/providers/event-api.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -7,12 +7,19 @@ import { IEvent } from 'src/types/event.interface';
 
 @Injectable()
 export class EventApiService {
-  private url = `${environment.apiHost}/event`;
+  private url = `${environment.apiHost}/events`;
 
   constructor(private http: HttpClient) {}
 
-  getEvents(): Observable<IEvent[]> {
-    return this.http.get<IEvent[]>(`${this.url}/all`);
+  getPaginatedEvents(
+    page: number,
+    limit: number
+  ): Observable<{ events: IEvent[]; totalItems: number }> {
+    const params = new HttpParams().set('page', page).set('limit', limit);
+
+    return this.http.get<{ events: IEvent[]; totalItems: number }>(this.url, {
+      params
+    });
   }
 
   getEventById(id: number): Observable<IEvent> {
