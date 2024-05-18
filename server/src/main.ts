2fc6f5c9b27c81754b203cefe112eaf5107aa2a3
Nazar <nazaranddobriy@gmail.com>
2024-05-18 16:34:58 +0300

    fix: change port

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,13 +4,13 @@ import { ValidationPipe } from '@nestjs/common/pipes';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
 
-  await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
+  await app.listen(PORT);
 }
 bootstrap();
